825051cc5b1b26ebae60e2b3879f1f83c98299a1
Aryan Grover <58443700+aryang23@users.noreply.github.com>
2022-01-20 11:26:23 +0530

    Create 189.Rotate Array.java

---
+++ Day 2/189.Rotate Array.java
@@ -0,0 +1,37 @@
+class Solution {
+    public void rotate(int[] nums, int k) {
+        int n = nums.length;
+        if(k==0 || n==1)
+            return;
+        if(k>n)
+            k=k%n;
+        
+        //Reverse 0 to n-1
+        reverse(nums, 0, n-1);
+        for(int val:nums)
+            System.out.print(val);
+        System.out.println();
+        
+        reverse(nums, 0, k-1);
+        for(int val:nums)
+            System.out.print(val);
+        System.out.println();
+        
+        reverse(nums, k, n-1);
+        for(int val:nums)
+            System.out.print(val);
+    }
+    
+    
+    public void reverse(int[] nums, int st, int end)
+    {
+        while(st<=end)
+        {
+            int temp = nums[st];
+            nums[st] = nums[end];
+            nums[end] = temp;
+            st++;
+            end--;
+        }
+    }
+}
